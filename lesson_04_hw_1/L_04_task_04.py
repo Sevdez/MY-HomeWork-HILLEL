5205b91834ec82bea79aabb761c0283080e9ea2b
Sevdez <112686493+Sevdez@users.noreply.github.com>
2022-09-20 23:25:56 +0300

    Update L_04_task_04.py

--- lesson_04_hw_1/L_04_task_04.py
+++ lesson_04_hw_1/L_04_task_04.py
@@ -3,8 +3,5 @@ i = float(input("Please enter an integer number:"))
 q = i+1
 w = i-1
 
-print("Next number for number", i, end="")
-print(" " + "is", q)
-
-print("Previous number for number", i, end="")
-print(" " + "is", w)
+print("Next number for number",  q)
+print("Previous number for number", w)
