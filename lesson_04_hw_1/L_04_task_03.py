77c89ec1911828628f6bbfb6007b5856691ff284
Sevdez <112686493+Sevdez@users.noreply.github.com>
2022-09-20 23:21:33 +0300

    Update L_04_task_03.py

--- lesson_04_hw_1/L_04_task_03.py
+++ lesson_04_hw_1/L_04_task_03.py
@@ -4,11 +4,9 @@ r = float(input("Введите радиус конуса: "))
 h = float(input("Введите высоту конуса: "))
 
 def cone_square_and_volume (a, b):
-    v = (math.pi*r*r*h)/3
+    v = math.pi*r*r*h/3
     s = math.pi*r*math.sqrt(r*r+h*h)
     return v, s
-
-cone_square_and_volume (r, h)
 
 v, s = cone_square_and_volume (r, h)
 
