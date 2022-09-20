69dbfe5b7d99db2880c63512262453569c0d85b1
Sevdez <112686493+Sevdez@users.noreply.github.com>
2022-09-20 23:18:19 +0300

    Update L_04_task_01.py

--- lesson_04_hw_1/L_04_task_01.py
+++ lesson_04_hw_1/L_04_task_01.py
@@ -3,10 +3,8 @@ import math
 a = float(input("Введите градусы:"))
 
 def degrees2radians (a):
-    q = (a*(math.pi)/180)
+    q = a*(math.pi)/180
     return q
-
-degrees2radians (a)
 
 g = degrees2radians(a)
 
